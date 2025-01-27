d9e77f250dd3877e77f45ffa461aaecd26af2fc2
Saidur289 <saidur.riaz1@gmailcomm>
2025-01-27 20:09:35 +0600

    set up my next project for dynamic route

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function () {
+  return (
+    <div>
+        <nav className='flex justify-center'>
+            <ul className="flex justify-between w-1/2">
+            <li><Link href={'/'}>HOME</Link></li>
+            <li><Link href={'/posts'}>POST</Link></li>
+            </ul>
+
+        </nav>
+    </div>
+  )
+}
